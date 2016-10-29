494fef0300eb154c10aa0df0acaf540c4988f101
Kiyoon Ko <kiyoonko@gmail.com>
2016-10-29 16:49:05 -0400

    Facebook API sucks. I'm done with it ugh

--- index.js
+++ index.js
@@ -10,24 +10,6 @@ var afternoon;
 var evening;
 var timeOfDay = 0;
 
-//facebook API
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '577670782440512',
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
