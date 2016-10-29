ab42e65b024382716c7196466c4abda72ae7b274
Kiyoon Ko <kiyoonko@gmail.com>
2016-10-29 11:51:24 -0400

    Custom case texts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const active = 0
+var active = 0
 
 app.set('port', (process.env.PORT || 5000))
 
