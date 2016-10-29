6bffa26e6656fca94d3c2a23c82ac1cc82874550
Geng <snggeng@gmailcom>
2016-10-29 18:45:17 -0400

    trying to access docEmotions

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/webhook/', function (req, res) {
                   console.log('error:', err);
                 }
                 else {
-                  mood = JSON.stringify(response, null, 2)
+                  mood = JSON.stringify(response, null, 2).docEmotions.anger
                   sendTextMessage('hi')
                   console.log(JSON.stringify(response, null, 2));
                 }
