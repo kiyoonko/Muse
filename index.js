6aeded735546c34f9bfbe88fdd96a2319abdaeee
Kiyoon Ko <kiyoonko@gmail.com>
2016-10-30 01:29:19 -0400

    authenticate button

--- index.js
+++ index.js
@@ -146,6 +146,9 @@ app.post('/webhook/', function (req, res) {
 			     sendTextMessage(sender, "Please say 'Hi MuseBot' to get started.")
             }
         }
+        if(event.postback){
+            authenticateSpotify();
+        }
     }
     res.sendStatus(200)
 })
@@ -191,3 +194,41 @@ function sendAction(sender) {
         }
     })
 }
+
+function authenticateButton(sender){
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload":{
+                "template_type":"button",
+                "text": "Check out your playlist!",
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Make Playlist",
+                    "payload": "run_auth_spotify"
+                }]
+            }
+        }   
+    }   
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    },  function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+function authenticateSpotify(){
+    console.log("Authenticate process");
+}
+
