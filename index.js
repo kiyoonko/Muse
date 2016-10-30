307afc0595fb95110325a1a217ff9ecac77141ba
Geng <snggeng@gmailcom>
2016-10-29 20:29:04 -0400

    testing keywords

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post('/webhook/', function (req, res) {
                   console.log('error:', err);
                 }
                 else {
-                  sendTextMessage(sender, "Your key words are: " + JSON.stringify(response, null, 2))
+                  sendTextMessage(sender, "Your key words are: " + JSON.stringify(response['keywords']))
                   console.log(response['docEmotions']);
                   }
               });
