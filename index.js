c0fef4c85e856cc7a7a737db3b7f0f3b0bc7173d
Kiyoon Ko <kiyoonko@gmail.com>
2016-10-30 00:24:24 -0400

    moving things around

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var active = 0
 var morning;
 var afternoon;
 var evening;
+var mood;
 var timeOfDay = 0;
 var watson = require('watson-developer-cloud');
 var alchemy_language = watson.alchemy_language({
@@ -59,7 +60,6 @@ app.post('/webhook/', function (req, res) {
         	timeOfDay = 0
         }
         else if(Boolean(active)){
-          var mood = ''
         	switch(timeOfDay){
         		case 0:
         			sendTextMessage(sender, "Hello! How is your morning?")
@@ -67,76 +67,74 @@ app.post('/webhook/', function (req, res) {
         			break;
         		case 1:
         			morning = text
-              var parameters = {
-                text: text
-              };
-
-              alchemy_language.emotion(parameters, function (err, response) {
-                if (err) {
-                  console.log('error:', err);
-                }
-                else {
-                  sendAction(sender)
-                  mood = JSON.stringify(response['docEmotions'])
-                  setTimeout(()=> { sendTextMessage(sender, "Your mood is:" + mood)}, 2000)
-                  }
-              });
-
-        			setTimeout(()=> { sendTextMessage(sender, "Hey! How is your afternoon?") }, 7000)
-        			timeOfDay = 2
-        			break;
+                    var parameters = {
+                        text: text
+                    };
+                    alchemy_language.emotion(parameters, function (err, response) {
+                        if (err) {
+                            console.log('error:', err);
+                        }
+                        else {
+                            sendAction(sender)
+                            mood = JSON.stringify(response['docEmotions'])
+                            setTimeout(()=> { sendTextMessage(sender, "Your mood is:" + mood)}, 2000)
+                        }
+                    });
+        		setTimeout(()=> { sendTextMessage(sender, "Hey! How is your afternoon?") }, 7000)
+        		timeOfDay = 2
+        		break;
 
         		case 2:
         			afternoon = text
-              var parameters = {
-                text: text
-              };
-              alchemy_language.emotion(parameters, function (err, response) {
-                if (err) {
-                  console.log('error:', err);
-                }
-                else {
-                  sendAction(sender)
-                  mood = JSON.stringify(response['docEmotions'])
-                  setTimeout(()=> { sendTextMessage(sender, "Your mood is:" + mood)}, 2000)
-                  }
-              });
+                    var parameters = {
+                        text: text
+                    };
+                    alchemy_language.emotion(parameters, function (err, response) {
+                        if (err) {
+                        console.log('error:', err);
+                    }
+                    else {
+                        sendAction(sender)
+                        mood = JSON.stringify(response['docEmotions'])
+                        setTimeout(()=> { sendTextMessage(sender, "Your mood is:" + mood)}, 2000)
+                    }
+                });
 
-        			setTimeout(()=> { sendTextMessage(sender, "Good evening~ How is your evening?")}, 7000)
-        			timeOfDay = 3
-        			break;
+        		setTimeout(()=> { sendTextMessage(sender, "Good evening~ How is your evening?")}, 7000)
+        		timeOfDay = 3
+        		break;
 
         		case 3:
         			evening = text
-              var parameters = {
-                text: text
-              };
+                    var parameters = {
+                        text: text
+                    };
 
-              alchemy_language.emotion(parameters, function (err, response) {
-                if (err) {
-                  console.log('error:', err);
-                }
-                else {
-                  sendAction(sender)
-                  mood = JSON.stringify(response['docEmotions'])
-                  setTimeout(()=> { sendTextMessage(sender, "Your mood is:" + mood)}, 2000)
-                  }
-              });
-              sendAction(sender)
+                    alchemy_language.emotion(parameters, function (err, response) {
+                        if (err) {
+                            console.log('error:', err);
+                        }
+                        else {
+                            sendAction(sender)
+                            mood = JSON.stringify(response['docEmotions'])
+                            setTimeout(()=> { sendTextMessage(sender, "Your mood is:" + mood)}, 2000)
+                        }
+                    });
+                    sendAction(sender)
         			setTimeout(()=> { sendTextMessage(sender, "Hmm... I see. Okay well here is a playlist created just for you based on your day except nah (At least not yet). Here are your responses - Morning: "+morning+" | Afternoon: "+afternoon+" | Evening "+evening+". Hope you enjoy the music!")}, 7000)
         			timeOfDay = 0
-					    active = 0
+					active = 0
         			console.log(timeOfDay, text, morning, afternoon, evening, active)
         			break;
-        	}
+        	    }
+            }
+            else{
+			     sendTextMessage(sender, "Please say 'Hi MuseBot' to get started.")
+            }
         }
-        else{
-			       sendTextMessage(sender, "Please say 'Hi MuseBot' to get started.")
-        }
-      }
     }
     res.sendStatus(200)
-  })
+})
 
 const token = "EAAINY3XI1EABAE7zY4GsUayrHSQVb9qD4WctZBYyEPeMHOle4lZAXhUkoIecYKfWqbPnV8PJ69woBpTeZCDhNBqXr46rBNZAar7KhJz0g5o1Yx91nJKpUqwi7rbDjkXj0rVlomdMeIf8XYOIaHGoZCjiUKNAmZCYqw90ah6W0HmwZDZD"
 
